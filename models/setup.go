ad1cbf348f4ea6f7f166d5ecea3abb7b2e2d2536
daffarakhap <pawitzy000@gmail.com>
2025-09-27 10:55:53 +0700

    i

--- models/setup.go
+++ models/setup.go
@@ -11,13 +11,19 @@ import (
 var DB *gorm.DB
 
 func ConnectDatabase() {
-	// Ambil URL database dari Railway (sudah include host, user, password, dbname, port)
-	dsn := os.Getenv("postgresql://postgres:inKAsaiCbAimbamplOoHsPSHGmcrNbcc@postgres.railway.internal:5432/railway")
-	if dsn == "" {
-		panic("DATABASE_URL environment variable is not set")
-	}
+	// Ambil variable dari Railway
+	host := os.Getenv("postgres.railway.internal")
+	user := os.Getenv("postgres")
+	password := os.Getenv("inKAsaiCbAimbamplOoHsPSHGmcrNbcc")
+	dbname := os.Getenv("railway")
+	port := os.Getenv("5432")
 
-	// Connect ke database
+	// Susun dsn sesuai format PostgreSQL
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Jakarta",
+		host, user, password, dbname, port,
+	)
+
 	database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic("Failed to connect to database: " + err.Error())
@@ -35,4 +41,5 @@ func ConnectDatabase() {
 
 	DB = database
 }
+
 
