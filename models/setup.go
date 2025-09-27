1eb9528af2b08553f19bea36e3fe20eb9152db6b
daffarakhap <pawitzy000@gmail.com>
2025-09-27 10:35:44 +0700

    connect raillway

--- models/setup.go
+++ models/setup.go
@@ -1,6 +1,9 @@
 package models
 
 import (
+	"fmt"
+	"os"
+
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
@@ -8,13 +11,25 @@ import (
 var DB *gorm.DB
 
 func ConnectDatabase() {
-	dsn := "host=localhost user=postgres password=1234567 dbname=mini-project port=5432 sslmode=disable TimeZone=Asia/Jakarta"
+	// Ambil variable dari Railway
+	host := os.Getenv("postgres.railway.internal")
+	user := os.Getenv("postgres")
+	password := os.Getenv("inKAsaiCbAimbamplOoHsPSHGmcrNbcc")
+	dbname := os.Getenv("railway")
+	port := os.Getenv("5432")
+
+	// Susun dsn sesuai format PostgreSQL
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable TimeZone=Asia/Jakarta",
+		host, user, password, dbname, port,
+	)
 
 	database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
-		panic(err)
+		panic("Failed to connect to database: " + err.Error())
 	}
 
+	// Auto migrate model
 	database.AutoMigrate(&Bioskop{})
 
 	DB = database
